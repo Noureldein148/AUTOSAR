0a9f8ac0c47a4701ce20c3cbe47f16e8a3c4ea44
Noureldein148 <1803671@eng.asu.edu.eg>
2023-12-28 23:00:44 +0200

    Autosar

--- Debug/Src/Det.d
+++
@@ -1,6 +0,0 @@
-Src/Det.o: ../Src/Det.c ../Inc/Det.h ../Inc/Platform_Types.h \
- ../Inc/Std_Types.h ../Inc/Platform_Types.h
-../Inc/Det.h:
-../Inc/Platform_Types.h:
-../Inc/Std_Types.h:
-../Inc/Platform_Types.h:
--- Debug/Src/Port_PBCFG.d
+++
@@ -1,8 +0,0 @@
-Src/Port_PBCFG.o: ../Src/Port_PBCFG.c ../Inc/Port.h \
- ../Inc/Platform_Types.h ../Inc/Port_Reg.h ../Inc/compiler.h \
- ../Inc/Std_Types.h
-../Inc/Port.h:
-../Inc/Platform_Types.h:
-../Inc/Port_Reg.h:
-../Inc/compiler.h:
-../Inc/Std_Types.h:
